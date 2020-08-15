206211f0315b1581b68f7939d3ac4509f6484c91
Tarang <tarangranpariya007@gmail.com>
2020-08-15 14:27:53 +0530

    reactstoreapp

---
+++ src/components/ProductList.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import Product from "./Product";
+import Title from './Title';
+// import { storeProducts } from "../data";
+import {  ProductConsumer } from "../context";
+
+class ProductList extends Component {
+    // state={
+    //     products: storeProducts
+    // };
+    render() {
+        // console.warn(this.state.products)
+        return (
+           <React.Fragment>
+               <div className="py-5">
+                    <div className="container">
+                        <Title name="Our" title="products" />
+                        <div className="row">
+                            <ProductConsumer>
+                                {value =>{
+                                    return value.products.map( product => {
+                                        return <Product key={product.id} product={product} />;
+                                    });
+                                }}
+                            </ProductConsumer>
+                        </div>
+                    </div>
+               </div>
+            </React.Fragment>
+        );
+    }
+}
+
+export default ProductList;
